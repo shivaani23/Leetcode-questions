4ef5e4d9b9de724ab8208d5feeaca9b2f5b805af
Shivaani <39997490+shivaani23@users.noreply.github.com>
2022-05-25 17:57:32 +0530

    Time: 3 ms (89.02%), Space: 7.4 MB (42.64%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -5,9 +5,7 @@ public:
         stack<int>st;
         int maxlen =0 , len =0;
         st.push(-1);
-        if(s == ""){
-            return 0;
-        }
+        
         for(int i=0;s[i]!='\0';i++){
             if(s[i] == '('){
                 st.push(i);
